acd401d047b5b5168253b5a41f900dd03bea97ed
sator-imaging <16752340+sator-imaging@users.noreply.github.com>
2025-12-16 12:36:06 +0900

    0.9.0

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@ import * as vscode from 'vscode';
 import { GitExtension, Repository } from './api/git';
 
 const ID: string = 'betterBranchNameDisplayView';
+const UNKNOWN: string = '<UNKNOWN>';
 
 // export function deactivate() { }
 
@@ -19,39 +20,35 @@ export async function activate(context: vscode.ExtensionContext) {
   }
 
   class Provider implements vscode.TreeDataProvider<string> {
-    // required for dynamic update
+    // members required for dynamic update
     private _onDidChange = new vscode.EventEmitter<void>();
     readonly onDidChangeTreeData = this._onDidChange.event;
 
-    readonly unknownBranchName: string = '<UNKNOWN>';
-    private branchName: string = this.unknownBranchName;
+    private readonly treeItem: vscode.TreeItem;
+    private branchName: string = UNKNOWN;
 
     constructor() {
-      git.repositories.forEach(repo => {
-        const subs = repo.state.onDidChange(() => this.onRepoChange(repo));
-        context.subscriptions.push(subs);
-
-        this.onRepoChange(repo);
-      });
-    }
-
-    private onRepoChange(repo: Repository) {
-      const name = repo.state.HEAD?.name;
-      if (name && name !== this.branchName) {
-        this.branchName = name;
-        this._onDidChange.fire();  // update UI automatically
-      }
+      const item = new vscode.TreeItem(UNKNOWN);
+      item.collapsibleState = vscode.TreeItemCollapsibleState.None;  // auto-fit
+      item.iconPath = new vscode.ThemeIcon('git-branch');
+      this.treeItem = item;
     }
 
     getTreeItem(element: string): vscode.TreeItem {
-      const item = new vscode.TreeItem(element);
-      item.collapsibleState = vscode.TreeItemCollapsibleState.None;  // auto-fit
-      item.iconPath = new vscode.ThemeIcon('git-branch');
-      return item;
+      if (element !== this.branchName) {
+        this.branchName = element;
+        this.treeItem.label = element;
+      }
+      return this.treeItem;
     }
 
     getChildren(): string[] {
       return [this.branchName];
+    }
+
+    setLabel(label: string) {
+      this.getTreeItem(label);
+      this._onDidChange.fire();  // update UI automatically
     }
   }
 
@@ -60,10 +57,23 @@ export async function activate(context: vscode.ExtensionContext) {
     treeDataProvider: provider
   });
 
-  treeView.title = 'HELLO';
-  treeView.message = 'WORLD';
+  treeView.title = UNKNOWN;
+  treeView.message = '👆 Current branch name is dynamically updated on git change';
 
-  // context.subscriptions.push(
-  //   vscode.window.registerTreeDataProvider(ID, provider)
-  // );
+  git.repositories.forEach(repo => {
+    const subs = repo.state.onDidChange(() => onRepoChange(repo));
+    context.subscriptions.push(subs);
+
+    onRepoChange(repo);
+  });
+
+  context.subscriptions.push(treeView);
+
+  function onRepoChange(repo: Repository) {
+    const name = repo.state.HEAD?.name;
+    if (name) {
+      treeView.title = name;
+      provider.setLabel(name);
+    }
+  }
 }
