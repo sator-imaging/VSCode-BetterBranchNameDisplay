56a48d88dd756cb14cb59a5469059d24e4b56e2e
sator-imaging <16752340+sator-imaging@users.noreply.github.com>
2025-12-16 10:08:38 +0900

    0.6.0

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,58 @@
-import * as vscode from "vscode";
-import { GitExtension, Repository } from "./api/git";
+import * as vscode from 'vscode';
+import { GitExtension, Repository } from './api/git';
 
-// export function deactivate() {}
+// export function deactivate() { }
 
 export async function activate(context: vscode.ExtensionContext) {
-  const gitExtension = vscode.extensions.getExtension<GitExtension>("vscode.git");
-  if (!gitExtension) {
-    vscode.window.showWarningMessage("Git extension not found");
-    return;
-  }
-
+  const gitExtension = vscode.extensions.getExtension<GitExtension>('vscode.git');
+  if (!gitExtension) return;
   if (!gitExtension.isActive) {
     await gitExtension.activate();
   }
 
   const git = gitExtension.exports.getAPI(1);
+  if (!git) {
+    vscode.window.showWarningMessage('Git extension not found');
+    return;
+  }
 
-  git.repositories.forEach(repo => {
-    const subs = repo.state.onDidChange(() => updateInputBoxPlaceholder(repo));
-    context.subscriptions.push(subs);
+  class Provider implements vscode.TreeDataProvider<string> {
+    // required for dynamic update
+    private _onDidChange = new vscode.EventEmitter<void>();
+    readonly onDidChangeTreeData = this._onDidChange.event;
 
-    updateInputBoxPlaceholder(repo);
-  });
+    readonly unknownBranchName: string = '<UNKNOWN>';
+    private branchName: string = this.unknownBranchName;
 
-  function updateInputBoxPlaceholder(repo: Repository) {
-    const branchName = repo.state.HEAD?.name ?? "<UNKNOWN>";
-    const inputBox = vscode.scm.inputBox;  // ok to use global source control
-    if (inputBox) {
-      inputBox.placeholder = `"${branchName}" Branch (Ctrl+Enter to commit)`;
+    constructor() {
+      git.repositories.forEach(repo => {
+        const subs = repo.state.onDidChange(() => this.onRepoChange(repo));
+        context.subscriptions.push(subs);
+
+        this.onRepoChange(repo);
+      });
+    }
+
+    private onRepoChange(repo: Repository) {
+      const name = repo.state.HEAD?.name;
+      if (name && name !== this.branchName) {
+        this.branchName = name;
+        this._onDidChange.fire();  // update UI automatically
+      }
+    }
+
+    getTreeItem(element: string): vscode.TreeItem {
+      const item = new vscode.TreeItem(element);
+      item.collapsibleState = vscode.TreeItemCollapsibleState.None;  // auto-fit
+      // item.iconPath = new vscode.ThemeIcon('git-branch');
+      return item;
+    }
+
+    getChildren(): string[] {
+      return [this.branchName];
     }
   }
+
+  const provider = new Provider();
+  vscode.window.registerTreeDataProvider('betterBranchNameDisplayView', provider);
 }
